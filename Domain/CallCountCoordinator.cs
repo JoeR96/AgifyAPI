eb586d7b843f7da452fdc346db746b6e4cfe49aa
JoeR96 <37408280+JoeR96@users.noreply.github.com>
2022-04-12 16:06:05 +0100

    Update CallCountCoordinator.cs

--- Domain/CallCountCoordinator.cs
+++ Domain/CallCountCoordinator.cs
@@ -6,17 +6,6 @@
         public static int CallsRemaining { get; set; }
         static CallCountCoordinator()
         {
-            //var callsRemaining = Persistence.PersistenceCoordinator.ReadCountFromTextFile();
-
-            //if(callsRemaining == string.Empty)
-            //{
-
-            //}
-            //else
-            //{
-            //    CallsRemaining = Convert.ToInt32(callsRemaining);
-            //}
-
             CallsRemaining = 1000;
         }
     }
